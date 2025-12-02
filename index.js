2e6100154ab10cd13b9527c419811525cdc17198
Ilyas-Ass <ilyasassjad123@gmail.com>
2025-12-02 22:32:35 +0700

    update profile user

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const userName = "Group 1";
-
+-
 console.log(`Hello, ${userName}! Welcome to the Group.`);
 
 const updateProfile = "user 1"
 
 console.log('Attempting to update profile with:', updateProfile)
+
+const updateProfile1 = "user 2"
+
+console.log('Attempting to update profile with:', updateProfile1)
